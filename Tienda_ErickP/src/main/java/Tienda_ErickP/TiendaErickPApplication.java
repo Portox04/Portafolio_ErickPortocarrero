a6e568be0b5e164e95e23a60d20e67afe13a30fc
Portox04 <dibtecportocarrero@gmail.com>
2026-01-22 09:21:05 -0600

    Semana #1

---
+++ Tienda_ErickP/src/main/java/Tienda_ErickP/TiendaErickPApplication.java
@@ -0,0 +1,13 @@
+package Tienda_ErickP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaErickPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaErickPApplication.class, args);
+	}
+
+}
---
+++ Tienda_ErickP/src/test/java/Tienda_ErickP/TiendaErickPApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_ErickP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaErickPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
