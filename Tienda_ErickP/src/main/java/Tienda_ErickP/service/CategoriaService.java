990dabada4862570e64fc565e819c8bd30438336
Portox04 <dibtecportocarrero@gmail.com>
2026-02-11 19:58:56 -0600

    Semana #4

--- Tienda_ErickP/src/main/java/Tienda_ErickP/service/CategoriaService.java
+++ Tienda_ErickP/src/main/java/Tienda_ErickP/service/CategoriaService.java
@@ -10,10 +10,14 @@ package Tienda_ErickP.service;
  */
 import Tienda_ErickP.domain.Categoria;
 import Tienda_ErickP.repository.CategoriaRepository;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CategoriaService {
@@ -27,6 +31,41 @@ public class CategoriaService {
             return categoriaRepository.findByActivoTrue();
         }
         return categoriaRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
+    }
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+
+    @Transactional
+    public void save(Categoria categoria, MultipartFile imageFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (!imageFile.isEmpty()) {
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imageFile, "categoria",
+                        categoria.getIdCategoria());
+                categoria.setRutaImagen(rutaImagen);
+                categoriaRepository.save(categoria);
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idCategoria) {
+        if (!categoriaRepository.existsById(idCategoria)) {
+            throw new IllegalArgumentException("La categoria con ID " + idCategoria + " no existe.");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("No se puede eliminar la categoria. Tiene datos asociados.", e);
+        }
     }
 
 }
