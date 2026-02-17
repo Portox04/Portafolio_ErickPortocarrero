4f6d7b3e05a7820619d00a73ed398b9d785200b4
Portox04 <dibtecportocarrero@gmail.com>
2026-02-16 20:39:17 -0600

    Semana #5 cambio de storage de Firebase

---
+++ Tienda_ErickP/src/main/java/Tienda_ErickP/service/ProductoService.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda_ErickP.service;
+
+/**
+ *
+ * @author porto
+ */
+import Tienda_ErickP.domain.Producto;
+import Tienda_ErickP.repository.ProductoRepository;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) {
+            return productoRepository.findByActivoTrue();
+        }
+        return productoRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+
+    @Transactional
+    public void save(Producto producto, MultipartFile imageFile) {
+        producto = productoRepository.save(producto);
+        if (!imageFile.isEmpty()) {
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imageFile, "producto",
+                        producto.getIdProducto());
+                producto.setRutaImagen(rutaImagen);
+                productoRepository.save(producto);
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idProducto) {
+        if (!productoRepository.existsById(idProducto)) {
+            throw new IllegalArgumentException("La producto con ID " + idProducto + " no existe.");
+        }
+        try {
+            productoRepository.deleteById(idProducto);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("No se puede eliminar la producto. Tiene datos asociados.", e);
+        }
+    }
+
+}
